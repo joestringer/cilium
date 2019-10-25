32f6286ef54cbebf0917618faba2a966bcc1abfc
Joe Stringer <joe@cilium.io>
2019-10-25 11:00:22 -0700

    tools: Add tool to introspect cgroup ids
    Signed-off-by: Joe Stringer <joe@cilium.io>

---
+++ tools/cgroup-id/main.go
@@ -0,0 +1,49 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/cilium/cilium/pkg/cgroups"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s <cgroup-path>\n", os.Args[0])
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usage()
+	}
+
+	path := os.Args[1]
+	_, err := os.Stat(path)
+	var raw_id uint64
+	if err == nil {
+		raw_id, err = cgroups.GetCgroupIDByPath(path)
+	} else {
+		raw_id, err = cgroups.GetCgroupID(path)
+	}
+	id := raw_id & 0xFFFFFFFF
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to get cgroup id: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stdout, "Cgroup %s has id %d (%#016x)\n", path, id, raw_id)
+}
