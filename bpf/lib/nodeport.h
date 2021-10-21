fc8469411a3e48c136c185a2683321451b711e21
Joe Stringer <joe@cilium.io>
2021-10-21 16:59:21 -0700

    bpf: Refactor node identity checking
    Create dedicated functions for checking whether an identity represents
    any node in the cluster or a remote node in the cluster. This will be
    useful for an upcoming commit where a remote node may have the
    REMOTE_NODE_ID identity or alternatively another hardcoded identity.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

--- bpf/lib/nodeport.h
+++ bpf/lib/nodeport.h
@@ -17,6 +17,7 @@
 #include "conntrack.h"
 #include "csum.h"
 #include "encap.h"
+#include "identity.h"
 #include "trace.h"
 #include "ghash.h"
 #include "pcap.h"
@@ -1139,7 +1140,7 @@ static __always_inline bool snat_v4_needed(struct __ctx_buff *ctx, __be32 *addr,
 		 * by the remote node if its native dev's
 		 * rp_filter=1.
 		 */
-		if (info->sec_label == REMOTE_NODE_ID)
+		if (identity_is_remote_node(info->sec_label))
 			return false;
 #endif
  #if defined(ENABLE_EGRESS_GATEWAY)
